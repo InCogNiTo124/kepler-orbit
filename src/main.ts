79309c2d8152bb5a62d3f2e0921a74745e7f5954
InCogNiTo124 <msmetko@msmetko.xyz>
2022-03-28 00:22:26 +0200

    Add RAAN indicator and interactivity

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import {planets} from './planets';
-
+import { planets } from './planets';
 
 import GUI from 'lil-gui'
-import { random16 } from 'three/src/math/MathUtils';
+
+
+
 
 interface OrbitState {
     a: number, // semi-major axis (units)
@@ -19,6 +20,63 @@ interface OrbitState {
 // https://stackoverflow.com/a/58326357
 const genRanHex = (size: number) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
 
+
+function createOrbit(orbitState: OrbitState): THREE.Object3D {
+    let root = new THREE.Object3D();
+    // yaw angle indicator
+    const yawAngleGeometry = new THREE.BufferGeometry();
+    const yawAngleMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+    const yawAngle = new THREE.LineLoop(yawAngleGeometry, yawAngleMaterial);
+    yawAngle.rotation.x = -Math.PI / 2.0;
+    root.add(yawAngle);
+
+    const yaw = new THREE.Object3D();
+    const pitch = new THREE.Object3D();
+    yaw.add(pitch);
+    root.add(yaw);
+
+    // pitch angle indicator
+    const pitchAngleCurve = new THREE.EllipseCurve(
+        0, 0,
+        2, 2,
+        0, 0,
+        false,
+        0);
+    const pitchAngleGeometry = new THREE.BufferGeometry();
+    const pitchAngleMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    const pitchAngle = new THREE.LineLoop(pitchAngleGeometry, pitchAngleMaterial);
+    pitchAngle.rotation.y = Math.PI / 2.0;
+    yaw.add(pitchAngle);
+
+    // periapsis indicator
+    const periapsisAngleCurve = new THREE.EllipseCurve(
+        0, 0,
+        2, 2,
+        0, 0,
+        false,
+        0);
+    const periapsisAngleGeometry = new THREE.BufferGeometry();
+    const periapsisAngleMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    const periapsisAngle = new THREE.LineLoop(periapsisAngleGeometry, periapsisAngleMaterial);
+    pitch.add(periapsisAngle);
+
+    // orbital plane
+    const orbitalPlaneGeo = new THREE.PlaneGeometry(25, 25);
+    const orbitalPlaneMat = new THREE.MeshBasicMaterial({ color: 0x339933, transparent: true, opacity: 0.2, side: THREE.DoubleSide });
+    const orbitalPlane = new THREE.Mesh(orbitalPlaneGeo, orbitalPlaneMat);
+    pitch.add(orbitalPlane);
+
+    // ellipse
+    const path = new THREE.Shape();
+    path.absellipse(0, 0, 4, 2, 0, 2 * Math.PI, false, 0);
+    const geometry = new THREE.ShapeBufferGeometry(path, 128);
+    const material = new THREE.MeshBasicMaterial({ color: 0x3f7b9d, side: THREE.DoubleSide });
+    const ellipse = new THREE.Mesh(geometry, material);
+    orbitalPlane.add(ellipse); // important
+
+    return root;
+}
+
 const state = {
     // a: 2.0, // semi-major axis (units)
     // e: 0.1, // eccentricity (scalar)
@@ -30,33 +88,59 @@ const state = {
     angles: "degrees",
     distances: "AU",
     showEcliptic: true,
-    newOrbit: function() {
+    newOrbit: function () {
         let folderName = "Orbit " + genRanHex(6);
         let newFolder = gui.addFolder(folderName);
-        let newOrbitState: OrbitState  = {
-            a: Math.random()*9+1,
-            e: Math.random()*(1-1e-7),
-            i: Math.random()*360,
-            O: Math.random()*360,
-            o: Math.random()*360,
+        let newOrbitState: OrbitState = {
+            a: Math.random() * 9 + 1,
+            e: Math.random() * (1 - 1e-7),
+            i: Math.random() * 360,
+            O: Math.random() * 360,
+            o: Math.random() * 360,
             theta: 0.0,
-            remove: function() {
-                let index = state.orbitStates.indexOf(newOrbitState);
-                state.orbitStates.splice(index, 1); // remove that orbit state
+            remove: function () {
+                let orbitStateIndex = state.orbitStates.indexOf(newOrbitState);
+                state.orbitStates.splice(orbitStateIndex, 1); // remove that orbit state
+
+                let orbitIndex = scene.children.indexOf(newOrbit);
+                scene.children.splice(orbitIndex, 1);
                 newFolder.destroy();
+                render();
                 // console.log('state.orbits.length', state.orbits.length);
             }
         };
         state.orbitStates.push(newOrbitState);
+        let newOrbit = createOrbit(newOrbitState);
         newFolder.add(newOrbitState, 'a', 0);
-        newFolder.add(newOrbitState, 'e', 0, 1-1e-7);
+        newFolder.add(newOrbitState, 'e', 0, 1 - 1e-7);
         newFolder.add(newOrbitState, 'i', 0, 360);
-        newFolder.add(newOrbitState, 'O', 0, 360).name("\u03A9");
+        newFolder.add(newOrbitState, 'O', 0, 360).name("\u03A9").onChange((e: number) => {
+            let yawAngle = newOrbit.children[0] as THREE.LineLoop;
+            let yaw = newOrbit.children[1];
+            let O = e * 2*Math.PI / 360;
+            // yaw indicator
+            const yawAngleCurve = new THREE.EllipseCurve(
+                0, 0,
+                2, 2,
+                0, 0,
+                false,
+                0);
+            yawAngleCurve.aEndAngle = O;
+            let points = yawAngleCurve.getPoints(50);
+            points.push(new THREE.Vector2(0.0, 0.0));
+            yawAngle.geometry.setFromPoints(points);
+            yaw.rotation.y = O;
+            render();
+        })
         newFolder.add(newOrbitState, 'o', 0, 360).name("\u03C9");
         newFolder.add(newOrbitState, 'remove').name("Remove");
+        // state.orbits.push(newOrbit);
+        scene.add(newOrbit);
+        render();
         // console.log('state.orbits.length', state.orbits.length)
     },
-    orbitStates: [] as OrbitState[]
+    orbitStates: [] as OrbitState[],
+    orbits: [] as THREE.Object3D[],
     // pos_x: 0.0,
     // pos_y: 0.0,
     // pos_z: 0.0,
@@ -68,10 +152,10 @@ const state = {
     // arg_y: 0.0,
 };
 
-const planetNames: { [key: string]: string}= {
+const planetNames: { [key: string]: string } = {
     Sun: "sol",
     Moon: "lun",
-    Earth: "ter", 
+    Earth: "ter",
     Saturn: "sat",
 }
 const gui = new GUI();
@@ -79,7 +163,7 @@ gui.add(state, 'planet', planetNames).onChange((e: string) => {
     createPlanet(celestialBody, e);
 }).name('Planet');
 gui.add(state, 'newOrbit').name('New orbit!')
-gui.add(state, 'showEcliptic').name('Show ecliptic plane').onChange((e: boolean) =>{
+gui.add(state, 'showEcliptic').name('Show plane').onChange((e: boolean) => {
     ecliptic.visible = e;
     render();
 })
@@ -90,8 +174,8 @@ gui.add(state, 'showEcliptic').name('Show ecliptic plane').onChange((e: boolean)
 // kepler.add(state, 'O', 0, 360).onChange(render).name("\u03A9");
 // kepler.add(state, 'o', 0, 360).onChange(render).name("\u03C9");
 const canvas: HTMLCanvasElement = document.querySelector("#canvas") as HTMLCanvasElement;
-const renderer = new THREE.WebGLRenderer({canvas});
-const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer({ canvas });
+const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
 
 camera.position.y = 2;
 camera.position.x = 3;
@@ -110,7 +194,7 @@ scene.add(celestialBody);
 // the invariable plane
 const planeGeom = new THREE.PlaneGeometry(25, 25);
 planeGeom.rotateX(-Math.PI * 0.5); // this is how you can do it
-const planeMat = new THREE.MeshBasicMaterial({color: 0xffffff, transparent: true, opacity: 0.2, side: THREE.DoubleSide});
+const planeMat = new THREE.MeshBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.2, side: THREE.DoubleSide });
 const ecliptic = new THREE.Mesh(planeGeom, planeMat);
 scene.add(ecliptic);
 
@@ -137,8 +221,8 @@ function render() {
         camera.updateProjectionMatrix();
     }
 
-    
-    renderer.render( scene, camera );
+
+    renderer.render(scene, camera);
 }
 controls.addEventListener('change', render);
 window.addEventListener('resize', render);
